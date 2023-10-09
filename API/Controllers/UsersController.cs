ad8f8bb1b760de8320d78e30356346bbc713184a
annatsherkavska <katotoka1985@gmail.com>
2023-10-09 17:28:37 +0200

    Start demoProject

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //api/users
+
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<IEnumerable<AppUser>> GetUser()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{Id}")]  //api/users/id
+    public async Task<AppUser> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+        SQLitePCL.Batteries.Init();
+    }
+    
+
+public DbSet<AppUser> Users { get; set;}
+
+}
